d73a78a3d616092d46ec620dce9b5e02d35cc2f1
MaryseLT <35494636+MaryseLT@users.noreply.github.com>
2021-01-12 10:23:01 -0500

    Additional Errors
    Added additional condition to try and catch ConnectError without crashing the script.

--- media_grab.py
+++ media_grab.py
@@ -13,7 +13,6 @@ import time
 import urllib.request
 import urllib.parse
 
-
 private_tweets = []
 dead_tweets = []
 stale_tweets = []
@@ -28,7 +27,7 @@ live_profiles = []
 def fetch_media_for_feed(feed_dict):
     start = datetime.now()
 
-    # Loop through folders
+    # Loop through my list of folders
     for feeds in feed_dict:
         short_name = feeds
         feeds = join(feed_dir, feeds)
@@ -91,34 +90,34 @@ def fetch_media_for_feed(feed_dict):
             for url in media_urls:
                 try:
                    status_code = urllib.request.urlopen(url)
-                except urllib.error.HTTPError as exception:
-                    if exception.getcode() !=200:
-                        if exception.getcode() ==403:
-                            print(exception, url)
-                            logger.info("Private tweet media: {0}".format(url))
+                except urllib.error.HTTPError as e:
+                    if e.getcode() !=200:
+                        if e.getcode() ==403:
+                            print(e, url)
+                            logger.info("PRIVATE Tweet Media > {0}".format(url))
                             private_tweets.append(url)
-                        elif exception.getcode() ==404:
-                            print(exception, url)
-                            logger.info("Dead tweet media: {0}".format(url))
+                        elif e.getcode() ==404:
+                            print(e, url)
+                            logger.info("DEAD Tweet Media > {0}".format(url))
                             dead_tweets.append(url)
                         else:
-                            logger.info("Tweet Media {0}: {1}".format(execption,url))
+                            logger.info("ERROR w/ Tweet Media > {0} {1}".format(execption,url))
                             dead_tweets.append(url)
 
             # Check if the image has already been downloaded
                 if media_urls[url] in os.listdir(image_dir):
-                    logger.info("{0} Tweet media has already been fetched: {1}".format(media_urls[url], url))
+                    logger.info("OLD Tweet Media > {0}".format(url))
                     stale_tweets.append(url)
                     # continue statement?
 
             # If the image hasn't already been downloaded
                 else:
-                    logger.info("{0} Fetching tweet media: {1}".format(short_name, url))
+                    logger.info("FETCHING Tweet Media > {0}".format(url))
                     media = s.get(url)
                     media_file = join(image_dir, media_urls[url])
                     with open(media_file, 'wb') as media_out:
                         media_out.write(media.content)
-                        print("Fetching Tweet Media: {0}".format(media_urls[url]))
+                        print("Fetching Tweet Media > {0}".format(media_urls[url]))
                     time.sleep(1)
                     live_tweets.append(url)
 
@@ -132,37 +131,41 @@ def fetch_media_for_feed(feed_dict):
             # Check if URL is live
                 try:
                     response = urllib.request.urlopen(url)
-                except urllib.error.HTTPError as exception:
-                    if exception.getcode() !=200:
-                        if exception.getcode() ==403:
-                            print(exception, url)
-                            logger.info("Private profile media: {0}".format(url))
+                except urllib.error.HTTPError as e:
+                    if e.getcode() !=200:
+                        if e.getcode() ==403:
+                            print(e, url)
+                            logger.info("PRIVATE Profile Media > {0}".format(url))
                             private_profiles.append(url)
-                        elif exception.getcode() ==404:
-                            print(exception, url)
-                            logger.info("Dead profile media: {0}".format(url))
+                        elif e.getcode() ==404:
+                            print(e, url)
+                            logger.info("DEAD Profile Media > {0}".format(url))
                             dead_profiles.append(url)
                         else:
-                            logger.info("Profile media {0}: {1}".format(exception,url))
+                            logger.info("ERROR w/ Profile Media > {0} {1}".format(e,url))
                             dead_profiles.append(url)
+                            #   Account for ConnectError
 
             # Check if ID folder exists
                 if not os.path.exists(profile_dir):
+                    logger.info("CREATNG profile_dir > {0}".format(profile_dir))
                     os.makedirs(profile_dir)
 
             # Check if image exists in ID folder
                 if filename not in os.listdir(profile_dir):
-                    logger.info("{0} Fetching profile media: {1}".format(short_name, url))
+                    logger.info("FETCHING Profile Media > {0}: {1}".format(profile_dir,url))
                     profile_image = s.get(url)
                     profile_image_file = join(profile_dir, filename)
                     with open(profile_image_file, 'wb') as profile_image_out:
                         profile_image_out.write(profile_image.content)
                     time.sleep(1)
-                    print("Fetching Profile Media: {0}".format(filename))
+                    print("FETCHING Profile Media > {0}".format(filename))
                     live_profiles.append(filename)
                 else:
                     stale_profiles.append(filename)
+    
     end = datetime.now()
+    logger.info("Finished Downloads > ", end)
     print("\nTotal Download Time:", end-start)
 
     return
